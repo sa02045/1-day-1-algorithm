f4ff3249f59dfaf786cccd63ea6477178fcf3734
DevJo <sa02045@naver.com>
2021-06-30 19:34:47 +0900

    신규아이디추천 코드 리팩토링

--- 프로그래머스/신규아이디추천.py
+++ 프로그래머스/신규아이디추천.py
@@ -2,11 +2,9 @@ def solution(s):
     min=len(s)
     for compp_len in range(1,len(s)//2 + 1):
         answer = ""
-        
-        compare_str = s[0:0+compp_len]
+        compare_str = s[0:compp_len]
         cnt = 1
-        for i in range(0,len(s),compp_len):
-
+        for i in range(compp_len,len(s),compp_len):
             if s[i:i+compp_len] == compare_str:
                 cnt += 1
             else :
@@ -16,7 +14,7 @@ def solution(s):
                     answer += str(cnt) + compare_str
                 cnt = 1
                 compare_str = s[i:i+compp_len]
-                
+
         if cnt == 1:    
             answer = answer+compare_str
         else :
