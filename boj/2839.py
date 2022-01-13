5fda640a3ad6443bbdff180f958d5675dd4b8bf4
DevJo <sa02045@naver.com>
2022-01-13 16:59:59 +0900

    2839 설탕배달 풀이완료

---
+++ boj/2839.py
@@ -0,0 +1,22 @@
+from asyncio.windows_events import INFINITE
+
+import math
+N=int(input())
+INFINITE = math.inf
+
+dp= [INFINITE for i in range(5001)]
+
+def sol(x):
+    dp[3]=1
+    dp[5]=1
+    for i in range(6,x+1):
+        dp[i] = min(dp[i-3],dp[i-5])+1
+
+    if dp[x] == INFINITE:
+        return -1
+    return dp[x]
+
+
+print(sol(N))
+
+
