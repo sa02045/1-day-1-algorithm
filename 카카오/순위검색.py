cfb9228808eaa00da32fb37018f756d65dbcbff8
DevJo <sa02045@naver.com>
2022-02-13 23:57:07 +0900

    카카오 순위검색

---
+++ 카카오/순위검색.py
@@ -0,0 +1,7 @@
+# query마다 info 배열을 탐색하며 조건에 맞는 답을 찾는 것은 느리다 (배열의 크기 전체를 탐색해야함)
+
+# 미리 info 테이블을 만든다
+
+# 문자열 형태로 "javabackendjuniorpizza"..? 문자열형태는 안좋다
+# ["java"]["backend"]["junior"]["pizza"] 4중배열
+
