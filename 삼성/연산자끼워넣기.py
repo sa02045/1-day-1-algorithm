896b5707a358a10640707d2c10c447a67bfba078
DevJo <sa02045@naver.com>
2022-02-24 23:35:55 +0900

    연산자끼워넣기

--- 삼성/연산자끼워넣기.py
+++ 삼성/연산자끼워넣기.py
@@ -1,54 +1,43 @@
 from itertools import permutations
+import math
 if __name__ == "__main__":
-    N = int(input())
-    Ai = list(map(int,input().split()))
+    N=int(input())
+    A=list(map(int,input().split()))
+    oper_num=list(map(int,input().split())) # N-1개의 연산자
+    operations = []
+    for _ in range((oper_num[0])):
+        operations.append('+')
+    for _ in range((oper_num[1])):
+        operations.append('-')
+    for _ in range((oper_num[2])):
+        operations.append('*')
+    for _ in range((oper_num[3])):
+        operations.append('/')
 
-    oper = list(map(int,input().split()))
-    
-    oper_list=[]
-    for i in range(len(oper)):
-        if oper[i] == 0:
-            continue
-        if i==0:
-            for _ in range(oper[i]):
-                oper_list.append('+')
-        elif i==1:
-            for _ in range(oper[i]):
-                oper_list.append('-')
-        elif i==2:
-            for _ in range(oper[i]):
-                oper_list.append('x')
-        elif i==3:
-            for _ in range(oper[i]):
-                oper_list.append('%')
+    max = -math.inf
+    min = math.inf
 
-    oper_list = (list(permutations(oper_list,len(oper_list))))
-    
-    max = -1000000001
-    min = 1000000001
-    sum=Ai[0]
-    tmp_oper=[]
-    for oper in oper_list:
-        for i in range(1,N):
-            if oper[i-1]=='+':
-                sum += Ai[i]
-            elif oper[i-1]=='-':
-                sum -= Ai[i]
-            elif oper[i-1]=='x':
-                sum *= Ai[i]
-            elif oper[i-1]=='%':
-                if sum <0:
-                    sum =- ( -sum // Ai[i])
+    for permu in list(permutations(operations,len(operations))):
+        sum = A[0]
+        for i in range(len(permu)):
+            if permu[i] == '+':
+                sum += A[i+1] 
+            if permu[i] == '-':
+                sum -= A[i+1]
+            if permu[i] == '*':
+                sum *= A[i+1]
+            if permu[i] == '/':
+                if sum < 0:
+                    sum = -(-sum // A[i+1])
                 else :
-                    sum = sum //Ai[i]
-
-        if max < sum:
+                    sum = sum // A[i+1]
+        
+        if sum > max:
             max = sum
-            tmp_oper =oper
-        if min > sum:
+        if sum < min :
             min = sum
-        sum=Ai[0]
+            
+    print((max))
+    print((min))
 
 
-print(max)
-print(min)
